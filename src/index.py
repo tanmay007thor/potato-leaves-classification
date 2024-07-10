564bb066e619256b010cee4782d3435c0857a665
tanmay007thor <tanmayrthod777@gmail.com>
2024-07-10 16:47:29 +0530

    changes

--- src/index.py
+++ src/index.py
@@ -1,54 +1,23 @@
-
-
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile 
 import uvicorn
-import numpy as np
+import numpy as np 
 from io import BytesIO
-from PIL import Image
-import tensorflow as tf
+from PIL import Image 
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-MODEL = tf.keras.models.load_model("../saved_model/1")
-
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
-
 @app.get("/ping")
 async def ping():
-    return "Hello, I am alive"
+    return "Hello world!"
 
-def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
+def read_file_as_image(data ) -> np.ndarray  :
+    
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    images = read_file_as_image(await file.read())
+    return 
 
 @app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
-):
-    image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
-    
-    predictions = MODEL.predict(img_batch)
-
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
-    return {
-        'class': predicted_class,
-        'confidence': float(confidence)
-    }
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
